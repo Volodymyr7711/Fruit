7f77758c3137b06d4cf2d98c96540f7112278b28
Volodymyr Mendyk <volodymyrmendyk70@gmail.com>
2023-03-31 18:20:50 +0200

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Volodymyr Mendyk on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
